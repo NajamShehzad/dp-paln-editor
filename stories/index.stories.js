bb89d767be874658621a7845a0ab403e338c7a34
NajamShehzad <najambutt195@gmail.com>
2019-06-17 16:38:25 +0500

    Story Book init

---
+++ stories/index.stories.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions'
+
+const stories = storiesOf('Storybook Knobs', module);
+
+// Add the `withKnobs` decorator to add knobs support to your stories.
+// You can also configure `withKnobs` as a global decorator.
+stories.addDecorator(withKnobs);
+
+// Knobs for React props
+stories
+  .addParameters({
+    options: {
+      name: 'Plan Editor',
+      isFullscreen: false,
+      showPanel: true,
+    }
+  })
+  .add('with a button', () => (
+    <button onClick={action("here")} >
+      {text('Label', 'Hello Storybook')}
+    </button>
+  ));
+
+// Knobs as dynamic variables.
+stories.add('as dynamic variables', () => {
+  const name = text('Name', 'Arunoda Susiripala');
+  const age = number('Age', 89);
+
+  const content = `I am ${name} and I'm ${age} years old.`;
+  return (<div>{content}</div>);
+});
